f73818161a4c5ab12dfd864c6fb9dcf1f9341fda
ks-santosh <159156207+SoulSantosh@users.noreply.github.com>
2024-04-05 23:09:49 +0100

    Add: obstacle and sidewalk pixel values array
    - added obstacle and sidewalk image pixel value array in Sprites.c file and declared in Sprites.h file

---
+++ UnCrash/UnCrashLibrary/Sprites.h
@@ -0,0 +1,23 @@
+/*
+ * Sprites.h
+ *
+ * Sprites used in the game
+ */
+
+#ifndef SPRITES_H_
+#define SPRITES_H_
+
+extern const unsigned short Sidewalk[1024];
+
+extern const unsigned short Obstacle1[4096];
+extern const unsigned short Obstacle2[4096];
+extern const unsigned short Obstacle3[4096];
+extern const unsigned short Obstacle4[4096];
+extern const unsigned short Obstacle5[4096];
+extern const unsigned short Obstacle6[4096];
+extern const unsigned short Obstacle7[4096];
+extern const unsigned short Obstacle8[4096];
+
+extern const unsigned short *const Obstacles[8];
+
+#endif /* SPRITES_H_ */
