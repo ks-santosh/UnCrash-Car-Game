af4412a45cf7d434e75be14dd78267b926c4f5ce
ks-santosh <164272377+ks-santosh@users.noreply.github.com>
2024-04-05 21:46:49 +0100

    Add: function to set world blocks
    - the function sets the world's basic building block design.
    - choses the obstacle type, the obstacle placement, and the coin placement.

--- UnCrash/UnCrashLibrary/RenderWorld.h
+++ UnCrash/UnCrashLibrary/RenderWorld.h
@@ -1,5 +1,21 @@
 //
+// Sets the design and placement of game world's basic blocks
+//
+typedef struct WorldBlock {
+	uint8_t ObsPlaceType;	// Obstacle placement type
+	uint8_t CoinPlaceType;	// Coin placement type
+	uint8_t ObsType1;
+	uint8_t ObsType2;
+
+} WorldBlock;
+
+//
 // Generates random number using Linear-Feedback Shift Register
 // - returns 16 bit random number
 //
 uint16_t GetRandomNumber();
+
+//
+//	Creates game world blocks to be rendered
+//	- no returns as the value is directly written to address
+void GetWorldSettings(WorldBlock *Block);
