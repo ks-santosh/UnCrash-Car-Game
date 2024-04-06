9accc60fbb32a86ed743e4e7704acdadf152d1b4
Bhaskarnikhilesh <159156251+Nikhilesh-Bhaskar@users.noreply.github.com>
2024-04-06 13:57:06 +0100

    Add: Initialised button address and structure
    initialised the push button base address and created a structure for controlling position of the car.

--- UnCrash/car_position.c
+++ UnCrash/car_position.c
@@ -1,3 +1,13 @@
 //car_position.c file
 #include "car_position.h"
 
+// Peripheral base addresses.
+volatile unsigned int *key_ptr = (unsigned int *)0xFF200050;
+
+// Define a structure to represent car position
+typedef struct {
+    int x;
+    int y;
+} CarPosition;
+
+
