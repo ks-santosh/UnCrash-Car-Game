ead610603f78d0efc07aab8feabda1644b40cfd3
Bhaskarnikhilesh <159156251+Nikhilesh-Bhaskar@users.noreply.github.com>
2024-04-06 14:31:09 +0100

    Fix: added the structures in header file.
    fix the error and added the structure in header file so that it can be accessible to other files.

--- UnCrash/car_position.h
+++ UnCrash/car_position.h
@@ -7,4 +7,17 @@
 int LT24_initialize_display(PLT24Ctx_t *lt24);
 int LT24_display_image(PLT24Ctx_t lt24, const unsigned short *image, int width, int height, int x, int y);
 
+// Define a structure to represent car position
+typedef struct {
+    int x;
+    int y;
+} CarPosition;
+
+// Define a structure to represent LT24 display context
+typedef struct {
+    PLT24Ctx_t lt24;
+    int width;
+    int height;
+} LT24Display;
+
 #endif /* CAR_POSITION_H */
