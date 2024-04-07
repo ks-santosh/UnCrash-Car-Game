b7f3d00c7fc9cc75878800688c110878f9eda992
ks-santosh <164272377+ks-santosh@users.noreply.github.com>
2024-04-07 23:58:55 +0100

    Add: Library empty files for render car functions

---
+++ UnCrash/UnCrashLibrary/RenderCar.c
@@ -0,0 +1,10 @@
+/*
+ * File: RenderCar.c
+ *
+ * Details : Renders the Car on LT24 screen at the given position
+ *
+ */
+
+#include "RenderCar.h"
+
+
---
+++ UnCrash/UnCrashLibrary/RenderCar.h
@@ -0,0 +1,9 @@
+#ifndef RENDERCAR_H_
+#define RENDERCAR_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "Sprites.h"
+#include "DE1SoC_LT24/DE1SoC_LT24.h"
+
+#endif /* RENDERCAR_H_ */
