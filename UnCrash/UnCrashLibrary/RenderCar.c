136761989dab81a48fa9b7f8d6ad3139fe9f717a
ks-santosh <159156207+SoulSantosh@users.noreply.github.com>
2024-04-08 02:59:52 +0100

    Add: function to render car
    - function renders car at a position
    - if car crashes then crashed car image is rendered.

--- UnCrash/UnCrashLibrary/RenderCar.c
+++ UnCrash/UnCrashLibrary/RenderCar.c
@@ -7,4 +7,29 @@
 
 #include "RenderCar.h"
 
+//
+// Renders the Car on LT24 screen at the given top right x and y position
+//
+void RenderCar(uint16_t PosX, uint16_t PosY, bool Crash, PLT24Ctx_t lt24) {
 
+	const unsigned short *ShowCar = Car;
+
+	if(Crash) {
+		ShowCar = CarCrashed;
+	}
+
+	// render car bonnet
+	LT24_copyFrameBuffer(lt24, ShowCar + 3, PosX+3, PosY, 10, 1);
+	LT24_copyFrameBuffer(lt24, ShowCar + 18, PosX+2, PosY+1, 12, 1);
+	LT24_copyFrameBuffer(lt24, ShowCar + 33, PosX+1, PosY+2, 14, 1);
+
+	// render car main body
+	LT24_copyFrameBuffer(lt24, ShowCar + 48, PosX, PosY+3, 16, 26);
+
+	// render car boot
+	LT24_copyFrameBuffer(lt24, ShowCar + 465, PosX+1, PosY+29, 14, 1);
+	LT24_copyFrameBuffer(lt24, ShowCar + 482, PosX+2, PosY+30, 12, 1);
+	LT24_copyFrameBuffer(lt24, ShowCar + 499, PosX+3, PosY+31, 10, 1);
+
+
+}
