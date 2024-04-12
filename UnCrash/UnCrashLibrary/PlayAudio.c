ba0bc52e5bd3b99895cd4ddcf9dbf99bdf17d10c
ks-santosh <164272377+ks-santosh@users.noreply.github.com>
2024-04-12 20:53:56 +0100

    Add: function to play music
    - function plays music from the sound file

--- UnCrash/UnCrashLibrary/PlayAudio.c
+++ UnCrash/UnCrashLibrary/PlayAudio.c
@@ -13,10 +13,10 @@
 void PlayCoinSound(PWM8731Ctx_t audio) {
 
 	// Sound duration
-	const int Duration = 10000;
+	const int32_t Duration = 10000;
 
 	// To store space of FIFO channels
-	unsigned int FifoSpace;
+	uint32_t FifoSpace;
 
 	// Get the sampleRate of the DAC
 	unsigned int sampleRate;
@@ -27,9 +27,9 @@ void PlayCoinSound(PWM8731Ctx_t audio) {
 	double inc1   = 988.0 * M_2PI / sampleRate;   	// Phase increment 1
 	double inc2   = 1319.0 * M_2PI / sampleRate;	// Phase increment 2
 	double ampl   = 8388608.0;						// Tone amplitude (i.e. volume)
-	signed int audio_sample = 0;
+	int32_t audio_sample = 0;
 
-	for(int i = Duration; i > 0; i--) {
+	for(int32_t i = Duration; i > 0; i--) {
 
 		//Always check the FIFO Space before writing to the left/right channel pointers
 		WM8731_getFIFOSpace(audio, &FifoSpace);
@@ -51,12 +51,39 @@ void PlayCoinSound(PWM8731Ctx_t audio) {
 			}
 
 			// Calculate next sample of the output tone.
-			audio_sample = (signed int)( ampl * sin( phase ) );
+			audio_sample = (int32_t)( ampl * sin( phase ) );
 
 			// Output tone to left and right channels.
 			WM8731_writeSample(audio, audio_sample, audio_sample);
 
 		}
+	}
+
+}
+
+//
+// Plays music
+//
+void PlayMusic(PWM8731Ctx_t audio) {
+
+	// To store space of FIFO channels
+	uint32_t FifoSpace;
+
+	uint32_t MusicSize = sizeof(MusicLoop)/sizeof(MusicLoop[0]);
+
+	for(int32_t i = 0; i < MusicSize; i++) {
+
+		//Always check the FIFO Space before writing to the left/right channel pointers
+		WM8731_getFIFOSpace(audio, &FifoSpace);
+
+		//If there is space in the write FIFO for both channels:
+		if (FifoSpace > 0) {
+			// Output tone to left and right channels.
+			WM8731_writeSample(audio, MusicLoop[i], MusicLoop[i]);
+		}
+
+		// To create delay between every sample
+		for(int8_t d=10; d>0;d--);
 	}
 
 }
--- UnCrash/UnCrashLibrary/PlayAudio.h
+++ UnCrash/UnCrashLibrary/PlayAudio.h
@@ -8,6 +8,10 @@
 #ifndef PLAYAUDIO_H_
 #define PLAYAUDIO_H_
 
+
+#include <stdint.h>
+#include <stdbool.h>
+
 // Libraries to set up audio drivers
 #include "DE1SoC_WM8731/DE1SoC_WM8731.h"
 #include "HPS_GPIO/HPS_GPIO.h"
@@ -18,9 +22,17 @@
 //Include Floating Point Math Libraries
 #include <math.h>
 
+// Contains music
+#include "Sounds.h"
+
 //
 // Plays two tones one after another
 //
 void PlayCoinSound(PWM8731Ctx_t audio);
+
+//
+// Plays music
+//
+void PlayMusic(PWM8731Ctx_t audio);
 
 #endif /* PLAYAUDIO_H_ */
