27e998a2334d69c02cf3898d593f22de53954951
ks-santosh <164272377+ks-santosh@users.noreply.github.com>
2024-04-12 19:24:04 +0100

    Add: sound files containing its value array
    - added music to be played at the beginning and end of the games.
    - music is sequence of signed int32 values stored in an array.

---
+++ UnCrash/UnCrashLibrary/Sounds.h
@@ -0,0 +1,12 @@
+/*
+ * File: Sounds.h
+ *
+ * Details: Variable declarations containing sound values.
+ */
+
+#ifndef SOUNDS_H_
+#define SOUNDS_H_
+
+extern const signed int MusicLoop[79447];
+
+#endif /* SOUNDS_H_ */
