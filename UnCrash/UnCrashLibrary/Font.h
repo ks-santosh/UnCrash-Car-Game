955b49de654ed2483ce073e4b4e6236525e250a3
ks-santosh <159156207+SoulSantosh@users.noreply.github.com>
2024-04-13 12:34:20 +0100

    Add: font pixel array
    - font file contains pixel array of capital letters and numbers.

---
+++ UnCrash/UnCrashLibrary/Font.h
@@ -0,0 +1,20 @@
+/*
+ * File: Font.h
+ *
+ * Details : Font variable declarations
+ *
+ */
+
+#ifndef FONT_H_
+#define FONT_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+
+// array of 2d pixel array image of alphabets
+extern const uint8_t *const Alphabet[26];
+
+// array of 2d pixel array of image of digits
+extern const uint8_t *const Digits[10];
+
+#endif /* FONT_H_ */
