ae6c27d8c4fe4c70031e2ca3a51b1a2729ab9815
ks-santosh <159156207+SoulSantosh@users.noreply.github.com>
2024-04-13 14:13:28 +0100

    Add: array of character widths
    - from array pointer sizeof operator won't work. So created a array of widths

--- UnCrash/UnCrashLibrary/Font.h
+++ UnCrash/UnCrashLibrary/Font.h
@@ -14,7 +14,13 @@
 // array of 2d pixel array image of alphabets
 extern const uint8_t *const Alphabet[26];
 
+// width of each letter
+extern const uint8_t AlWidth[26];
+
 // array of 2d pixel array of image of digits
 extern const uint8_t *const Digits[10];
+
+// width of each digit
+extern const uint8_t DiWidth[10];
 
 #endif /* FONT_H_ */
