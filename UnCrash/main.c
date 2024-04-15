58fd9770b972cac7e5b8a02f242d3ef323d805e5
ks-santosh <159156207+SoulSantosh@users.noreply.github.com>
2024-04-15 18:16:17 +0100

    Fix: game speed depends on compiler optimization
    Issue: game speed increases if compiled with higher optimization levels
    Fix: Used ARM A9 private timer to set a maximum task rate

--- UnCrash/main.c
+++ UnCrash/main.c
@@ -26,7 +26,9 @@
 #include "UnCrashLibrary/RenderText.h"
 
 void exitOnFail(signed int status, signed int successStatus){
-    if (status != successStatus) {
+    static unsigned int taski = 0;
+	if (status != successStatus) {
+		taski++;
         exit((int)status); //Add breakpoint here to catch failure
     }
 }
@@ -53,6 +55,16 @@ int main(void) {
     exitOnFail(HPS_GPIO_initialise(LSC_BASE_ARM_GPIO, ARM_GPIO_DIR, ARM_GPIO_I2C_GENERAL_MUX, 0, &gpio), ERR_SUCCESS);
     exitOnFail(HPS_I2C_initialise(LSC_BASE_I2C_GENERAL, I2C_SPEED_STANDARD, &i2c), ERR_SUCCESS);
     exitOnFail(WM8731_initialise(LSC_BASE_AUDIOCODEC, i2c, &audio), ERR_SUCCESS);
+
+    //Initialise the ARM A9 Private Timer
+    volatile unsigned int *private_timer_load = (unsigned int *)(LSC_BASE_PRIV_TIM + 0x0);
+    volatile unsigned int *private_timer_value = (unsigned int *)(LSC_BASE_PRIV_TIM + 0x4);
+	volatile unsigned int *private_timer_control = (unsigned int *)(LSC_BASE_PRIV_TIM + 0x8);
+	volatile unsigned int *private_timer_interrupt = (unsigned int *)(LSC_BASE_PRIV_TIM + 0xC);
+	*private_timer_load      = 0xFFFFFFFF;
+	*private_timer_control   = (0 << 8) | (0 << 2) | (1 << 1) | (1 << 0);
+	uint32_t TaskTimerValue = *private_timer_value;
+	uint32_t CurrentTimerValue = *private_timer_value;
 
     //Clear both FIFOs
     WM8731_clearFIFO(audio, true,true);
@@ -94,40 +106,55 @@ int main(void) {
 		// Play music before game begins
 		PlayMusic(audio);
 
+		// Play loop
 		while (1) {
 
-			// reset watchdog timer
-			HPS_ResetWatchdog(); //Just reset the watchdog.
+			CurrentTimerValue = *private_timer_value;
 
-			// render the world
-			RenderWorld(WBlocks, lt24, WorldShiftY);
+			// The time controls the speed of the game
+			if((TaskTimerValue - CurrentTimerValue) >= SP_TASK) {
 
-			// move the car on button press
-			CarPosX = (uint16_t)MoveCar((int16_t)CarPosX, CarShiftX, CollSts.Crash,lt24);
+				TaskTimerValue = *private_timer_value;
 
-			// check for crash or coins
-			CheckCollision(WBlocks, CarPosX, &CollSts);
+				// reset watchdog timer
+				HPS_ResetWatchdog();
 
-			// if crash detected break from the loop
-			if(CollSts.Crash) {
-				break;
-			}
+				// render the world
+				RenderWorld(WBlocks, lt24, WorldShiftY);
 
-			// if score updated display it on Seven-Segment
-			if(PrvScore != CollSts.Score) {
-				SetSevenSegDisp(CollSts.Score);
+				// move the car on button press
+				CarPosX = (uint16_t)MoveCar((int16_t)CarPosX, CarShiftX, CollSts.Crash,lt24);
 
-				// Play coin sound effect
-				PlayCoinSound(audio);
+				// check for crash or coins
+				CheckCollision(WBlocks, CarPosX, &CollSts);
 
-				PrvScore = CollSts.Score;
-
-				// game ends if max score achieved
-				if(CollSts.Score == MAX_SCORE) {
+				// if crash detected break from the loop
+				if(CollSts.Crash) {
 					break;
 				}
+
+				// if score updated display it on Seven-Segment
+				if(PrvScore != CollSts.Score) {
+					SetSevenSegDisp(CollSts.Score);
+
+					// Play coin sound effect
+					PlayCoinSound(audio);
+
+					PrvScore = CollSts.Score;
+
+					// game ends if max score achieved
+					if(CollSts.Score == MAX_SCORE) {
+						break;
+					}
+				}
 			}
-		}
+
+			// Clear the private timer interrupt flag if it is set
+			if (*private_timer_interrupt & 0x1) {
+				*private_timer_interrupt = 0x1;
+			}
+
+		} // Play loop end
 
 		// Play music after game ends
 		PlayMusic(audio);
