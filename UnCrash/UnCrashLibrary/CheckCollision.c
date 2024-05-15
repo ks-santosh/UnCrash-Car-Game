b4b45c6062d6cdf876cff48d6b67c6a946cd25ca
ks-santosh <159156207+SoulSantosh@users.noreply.github.com>
2024-05-15 14:25:18 +0100

    Docs: add comments in CheckCollision library
    - Added descriptive comments in CheckCollision library.
    - The library is used to detect collisions between the player's car and other objects within the game world.

--- UnCrash/UnCrashLibrary/CheckCollision.c
+++ UnCrash/UnCrashLibrary/CheckCollision.c
@@ -1,127 +1,173 @@
 /*
  * File: CheckCollision.c
  *
- * Details : Checks for collision between car and other objects in the world
+ * Details : Detects collisions between the player's car
+ * and other objects within the game world.
+ *
+ * Author: Santosh K S
  *
  */
+
+// Header inclusion
 #include "CheckCollision.h"
 
 //
-// Finds the pixel value of the world ground at the give coordinates
-// and based on it's value it updates the crash status or Score
+// Function: SetCollisionAction
+// Details: Determines the pixel value of the game world at the specified coordinates,
+// updating the crash status or score based on its value.
+// Arguments: World blocks, x-y coordinates, collision status
+// Return: Void. Updates the collision status value passed in argument.
 //
 void SetCollisionAction(WorldBlock WBlocks[], uint16_t Px, uint16_t Py, CollisionEvent *CollSts) {
 
-	// Pixel
-	uint16_t Pixel = 0x32D0; // colour of road
+	// Pixel colour
+	uint16_t Pixel = 0x32D0; // Colour of road
 
-	// shifting the origin after sidewalk
-	Px = Px - SW_WIDTH;
+	// Shifting the X-origin after sidewalk
+	Px = Px - SW_WIDTH; // Subtract sidewalk width
 
-	// Find which of the three boxes of the WorldBlocks the point falls in
-	uint8_t Box = Px >> 6;	// Px divide by 64
+	// Find which of the three box of the WorldBlocks the point falls in
+	uint8_t Box = Px >> 6;	// Px divided by (2^6) 64 which is the box width
 
-	// X value relative to the obstacle box
+	// Shifting origin of X relative to the box
 	Px = Px - Box*OB_SIDE;
 
-	// give the ObsType index
+	// Index of obstacle type ObsType
 	uint8_t ObsId = Box;
 
-	// bits in box represent the position
+	// Bits in box represent the position
 	Box = (1 << Box);
 
-	// flag to check if car collided with obstacle or coin
-	bool CollType = false; // false : Coin true : Obstacle
+	// Flag to check if car collided with obstacle or coin
+	bool CollType = false; // True: Obstacle False: Coin
 
 	// To track the block id where point lies
 	bool WBlockColl = false; // false - collision happened in WBlocks[1]
 
-	// Check in WBlock1
+	// Check in WBlock1 - second block from bottom
+	// if Y point falls inside it
 	if(Py >= WBlocks[1].OffsetY) {
-		// if box has obstacles
+
+		// Shift origin of Y to WBlock1
 		Py = Py - WBlocks[1].OffsetY;
+
+		// If box has obstacles
 		if(Box & WBlocks[1].ObsPlaceType) {
+
+			// Get the pixel value at the coordinate
 			Pixel = Obstacles[WBlocks[1].ObsType[ObsId]][Py*64 + Px];
-			CollType = true;
+			CollType = true; // True: Obstacle False: Coin
 		}
-		// if box has coins
+
+		// If box has coins
 		else if(Box & WBlocks[1].CoinPlaceType) {
+
+			// Get the pixel value at the coordinate
 			Pixel = Coin[Py*64 + Px];
 		}
 	}
-	// Check in WBlock2
+
+	// Check in WBlock2 - third block from bottom otherwise
 	else {
 
-		WBlockColl = true;
+		WBlockColl = true;  // true - collision happened in WBlocks[2]
+
+		// Shift origin of Y to WBlock2
 		Py = Py - WBlocks[2].OffsetY;
 
-		// if box has obstacles
+		// If box has obstacles
 		if(Box & WBlocks[2].ObsPlaceType) {
 
+			// Get the pixel value at the coordinate
 			Pixel = Obstacles[WBlocks[2].ObsType[ObsId]][Py*64 + Px];
-			CollType = true;
+
+			CollType = true; // True: Obstacle False: Coin
 		}
-		// if box has coins
+		// If box has coins
 		else if(Box & WBlocks[2].CoinPlaceType) {
+			// Get the pixel value at the coordinate
 			Pixel = Coin[Py*64 + Px];
 		}
 	}
 
-	// A point of car is not on road
+	// The point doesn't fall on road if
+	// the pixel value doesn't match the road's colour
 	if(Pixel != ROAD_COLOUR) {
-		if(CollType) {
-			CollSts->Crash = true;
-		}
-		// if collision is with coin
-		else {
-			CollSts->Crash = false;
-			CollSts->Score++;
 
-			// remove the coin from the block
-			if(WBlockColl) { // remove coin from WBlocks[2]
+		// If collision with obstacle
+		// True: Obstacle False: Coin
+		if(CollType) {
+			CollSts->Crash = true; // Set crash status
+		}
+
+		// If collision with coin
+		else {
+
+			CollSts->Crash = false; // reset crash status
+
+			CollSts->Score++; // Increment score
+
+			// Remove the coin from the block
+			// if collision happened in block2
+			if(WBlockColl) {
+				// Remove coin from WBlocks[2]
 				WBlocks[2].CoinPlaceType = (~Box) & WBlocks[2].CoinPlaceType;
 			}
 			else {
+				// Remove coin from WBlocks[1]
 				WBlocks[1].CoinPlaceType = (~Box) & WBlocks[1].CoinPlaceType;
 			}
 		}
 	}
 
 }
+
 //
-// Checks if the points of car collides with obstacles or coin
+// Function: CheckCollision
+// Details: Verifies if each corner of the car collides with obstacles
+// or coins, updating the score in case of a coin collision and setting
+// the crash status upon collision with obstacles.
+// Arguments: World blocks, car position and collision status
+// Return: Void. Updates the collision status value passed in argument.
 //
 void CheckCollision(WorldBlock WBlocks[], uint16_t CarPosX, CollisionEvent *CollSts) {
 
-	static uint16_t PrvScore = 0; // previous score
+	static uint16_t PrvScore = 0; // Previous score
 
-	// check car's top-left corner
+	// Check car's top-left corner
 	SetCollisionAction(WBlocks, CarPosX+2, CAR_POS_Y+2, CollSts);
+	// If crash or change in score
 	if((CollSts->Crash) || (PrvScore != CollSts->Score)) {
+		// Update previous score
 		PrvScore = CollSts->Score;
 		return;
 	}
 
-	// check car's bottom-right corner
+	// Check car's bottom-right corner
 	SetCollisionAction(WBlocks, CarPosX + CAR_WIDTH - 3, CAR_POS_Y + CAR_HEIGHT - 3,CollSts);
+	// If crash or change in score
 	if((CollSts->Crash) || (PrvScore != CollSts->Score)) {
+		// Update previous score
 		PrvScore = CollSts->Score;
 		return;
 	}
 
-	// check car's top-right corner
+	// Check car's top-right corner
 	SetCollisionAction(WBlocks, CarPosX + CAR_WIDTH - 3, CAR_POS_Y + 2,CollSts);
+	// If crash or change in score
 	if((CollSts->Crash) || (PrvScore != CollSts->Score)) {
+		// Update previous score
 		PrvScore = CollSts->Score;
 		return;
 	}
 
-	// check car's bottom-left corner
+	// Check car's bottom-left corner
 	SetCollisionAction(WBlocks, CarPosX + 2, CAR_POS_Y + CAR_HEIGHT - 3,CollSts);
+	// If crash or change in score
 	if((CollSts->Crash) || (PrvScore != CollSts->Score)) {
+		// Update previous score
 		PrvScore = CollSts->Score;
 		return;
 	}
-
 
 }
--- UnCrash/UnCrashLibrary/CheckCollision.h
+++ UnCrash/UnCrashLibrary/CheckCollision.h
@@ -1,36 +1,52 @@
 /*
  * File: CheckCollision.h
  *
- * Details : Checks for collision between car and other objects in the world
+ * Details : Detects collisions between the player's car
+ * and other objects within the game world.
+ *
+ * Author: Santosh K S
  *
  */
 
+// Include guards to prevent multiple header inclusions
 #ifndef CHECKCOLLISION_H_
 #define CHECKCOLLISION_H_
 
-#include <stdint.h>
-#include <stdbool.h>
-#include "Sprites.h"
+// Header inclusion
+// Standard C library
+#include <stdint.h> // C bool Library
+#include <stdbool.h> // C header for integer types with specific widths
+
+#include "Sprites.h" // Game sprite dimensions and game world definition
 
 // Maximum score achievable in game
 #define MAX_SCORE 9999
 
 //
-// Stores the action of collision event: crash or collect coin
+// Structure: CollisionEvent
+// Details: Stores the action of collision event: crash or collect coin
 //
 typedef struct CollisionEvent {
-	bool Crash;		// true if car crashes with obstacles
-	uint16_t Score;	// keeps track of the coins collected
+	bool Crash;		// True if car crashes with obstacles
+	uint16_t Score;	// Keeps track of the coins collected
 } CollisionEvent;
 
 //
-// Finds the pixel value of the world ground at the give coordinates
-// and based on it's value it updates the crash status or Score
+// Function: SetCollisionAction
+// Details: Determines the pixel value of the game world at the specified coordinates,
+// updating the crash status or score based on its value.
+// Arguments: World blocks, x-y coordinates, collision status
+// Return: Void. Updates the collision status value passed in argument.
 //
 void SetCollisionAction(WorldBlock WBlocks[], uint16_t Px, uint16_t Py, CollisionEvent *CollSts);
 
 //
-// Checks if the points of car collides with obstacles or coin
+// Function: CheckCollision
+// Details: Verifies if each corner of the car collides with obstacles
+// or coins, updating the score in case of a coin collision and setting
+// the crash status upon collision with obstacles.
+// Arguments: World blocks, car position and collision status
+// Return: Void. Updates the collision status value passed in argument.
 //
 void CheckCollision(WorldBlock WBlocks[], uint16_t CarPosX, CollisionEvent *CollSts);
 
